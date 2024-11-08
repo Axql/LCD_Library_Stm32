29576b082e92be5d831136728970519f6b3233b9
Axql <113419922+Axql@users.noreply.github.com>
2024-11-08 15:02:09 +0100

    Final version of my lcd library for now
    i added comments to this and a function to turn the lcd screen on

--- lcd.c
+++ lcd.c
@@ -4,18 +4,23 @@
  *  Created on: Oct 15, 2024
  *      Author: axel
  */
+#include <lcd.h>
 #include <stdio.h>
-#include "lcd.h"
 
+//making the variables for the library
 GPIO_TypeDef *lcdport;
 GPIO_TypeDef *lcdEnport;
 GPIO_TypeDef *lcdRport;
+
 uint16_t lcdRSPin;
 uint16_t lcdENPin;
 uint16_t lcdDataPin[4];
 
-void lcdSetup(GPIO_TypeDef *PORT,GPIO_TypeDef *ENPORT, GPIO_TypeDef *RPORT, uint16_t RS, uint16_t EN, uint16_t D4, uint16_t D5, uint16_t D6, uint16_t D7)
-{
+
+//the setup function to set all the variables and all the pins
+void lcdSetup(GPIO_TypeDef *PORT, GPIO_TypeDef *ENPORT, GPIO_TypeDef *RPORT,
+		uint16_t RS, uint16_t EN, uint16_t D4, uint16_t D5, uint16_t D6,
+		uint16_t D7) {
 	lcdport = PORT;
 	lcdEnport = ENPORT;
 	lcdRport = RPORT;
@@ -27,45 +32,40 @@ void lcdSetup(GPIO_TypeDef *PORT,GPIO_TypeDef *ENPORT, GPIO_TypeDef *RPORT, uint
 	lcdDataPin[3] = D7;
 }
 
-void lcdEnable ()
-{
+//this function is to send the data to the screen
+void lcdEnable() {
 	HAL_GPIO_WritePin(lcdEnport, lcdENPin, SET);
 	HAL_Delay(1);
 	HAL_GPIO_WritePin(lcdEnport, lcdENPin, RESET);
 }
 
-
-void lcdWrite(uint8_t data)
-{
-	  lcdSendNibble(data >> 4);  // Send the upper nibble first
-	  lcdSendNibble(data & 0x0F);
+//this function will send the nibbles to the screen
+void lcdWrite(uint8_t data) {
+	lcdSendNibble(data >> 4);  // Send the upper nibble first
+	lcdSendNibble(data & 0x0F);
 }
 
-void lcdSendNibble(uint8_t nibble)
-{
-    for (int i = 0; i < 4; i++)  // Iterate over the 4 data pins (D4 to D7)
-    {
-        if (nibble & (1 << i))
-        {
-            HAL_GPIO_WritePin(lcdport, lcdDataPin[i], GPIO_PIN_SET);  // Set bit
-        }
-        else
-        {
-            HAL_GPIO_WritePin(lcdport, lcdDataPin[i], GPIO_PIN_RESET);  // Clear bit
-        }
-    }
-    lcdEnable();  // Toggle the enable pin to send the data
+//this function send the nibbles to the write command
+void lcdSendNibble(uint8_t nibble) {
+	for (int i = 0; i < 4; i++)  // Iterate over the 4 data pins (D4 to D7)
+			{
+		if (nibble & (1 << i)) {
+			HAL_GPIO_WritePin(lcdport, lcdDataPin[i], GPIO_PIN_SET);  // Set bit
+		} else {
+			HAL_GPIO_WritePin(lcdport, lcdDataPin[i], GPIO_PIN_RESET); // Clear bit
+		}
+	}
+	lcdEnable();  // Toggle the enable pin to send the data
 }
 
-
-void lcdCommand(uint8_t command)
-{
+//this is a function to send commands to the screen
+void lcdCommand(uint8_t command) {
 	HAL_GPIO_WritePin(lcdRport, lcdRSPin, RESET);
 	lcdWrite(command);
 }
 
-void lcdInit()
-{
+// this function will make sure the display is properly initialized
+void lcdInit() {
 	lcdCommand(0x03);
 	HAL_Delay(10);
 	lcdCommand(0x03);
@@ -81,109 +81,122 @@ void lcdInit()
 
 }
 
-void lcdChar(uint8_t ch)
-{
+//this function will print a character on the screen
+void lcdChar(uint8_t ch) {
 	HAL_GPIO_WritePin(lcdRport, lcdRSPin, SET);
 	lcdWrite(ch);
 }
 
-
-void lcdString(char * string)
-{
-	while(*string)
+//this function prints a string on the screen
+void lcdString(char *string) {
+	while (*string)
 		lcdChar(*string++);
 }
 
-
-void lcdWriteInt(char * format, uint32_t number)
-{
+//this function prints an int on the screen
+void lcdWriteInt(char *format, uint32_t number) {
 	char buffer[20];
 	sprintf(buffer, format, number);
 	lcdString(buffer);
 }
 
-
-void lcdWriteFloat(char * format, double number)
-{
+//this function will print a float on the screen
+void lcdWriteFloat(char *format, double number) {
 	char buffer[20];
 	sprintf(buffer, format, number);
 	lcdString(buffer);
 }
 
-
-void lcdSetCursor(uint8_t row, uint8_t col)
-{
-	switch(row)
-	{
-		case 0:
-			lcdWrite(0x80+col);
+//this function enables you to set the cursor wherever you want on the screen
+void lcdSetCursor(uint8_t row, uint8_t col) {
+	switch (row) {
+	case 0:
+		lcdWrite(0x80 + col);
 		break;
-		case 1:
-			lcdWrite(0xC0+col);
+	case 1:
+		lcdWrite(0xC0 + col);
 		break;
 	}
 }
 
-void lcdCustomCharacter(uint8_t location, uint8_t charmap[])
-{
-	 location &= 0x07;
+//this function creates a custom character
+void lcdCustomCharacter(uint8_t location, uint8_t charmap[]) {
+	location &= 0x07;
 
-	 lcdCommand(0x40 | (location << 3));
+	lcdCommand(0x40 | (location << 3));
 
-	 for (int i = 0; i < 8; i++)
-	 {
-	         lcdChar(charmap[i]);
-	 }
+	for (int i = 0; i < 8; i++) {
+		lcdChar(charmap[i]);
+	}
 }
 
-void lcdClear()
+
+//this function converts a number to binary and prints it on the screen
+void lcdPrintBinary(uint32_t number, uint32_t bits)
 {
+	char binStr[bits+1];
+	binStr[bits] = '\0';
+
+	 for (int i = bits-1; i >= 0; i--) {
+	        binStr[i] = (number & 1) ? '1' : '0';
+	        number >>= 1;
+	    }
+	 lcdString(binStr);
+}
+
+
+//this function will clear the screen
+void lcdClear() {
 	lcdCommand(0x01);
 }
 
-void lcdCursorOFF()
-{
+//this function will turn off the cursor
+void lcdCursorOFF() {
 	lcdCommand(0x0C);
 }
 
-void lcdCursorON()
-{
+//this function turns the cursor back on
+void lcdCursorON() {
 	lcdCommand(0x0E);
 }
 
-void lcdCursorBlink()
-{
+//this function will toggle the blinking of the cursor
+void lcdCursorBlink() {
 	lcdCommand(0x0F);
 }
 
-void lcdCursorHome()
-{
+//this function will return the cursor to the home position
+void lcdCursorHome() {
 	lcdCommand(0x02);
 }
 
-void lcdShiftRight()
-{
+//this function will shift the display right by 1 spot
+void lcdShiftRight() {
 	lcdCommand(0x1E);
 }
 
-void lcdShiftLeft()
-{
+//this function shifts the display to the left by 1 spot
+void lcdShiftLeft() {
 	lcdCommand(0x18);
 }
 
-void lcdOFF()
-{
+//this function turns the display off
+void lcdOFF() {
 	lcdCommand(0x08);
 }
-void lcdCursorLeft()
-{
+
+//this function turns the display back on
+void lcdON() {
+	lcdCommand(0x0C);
+}
+
+//this function shifts the cursor left by one spot
+void lcdCursorLeft() {
 	lcdCommand(0x10);
 }
-void lcdCursorRight()
-{
+
+//this function shifts the cursor right by one spot
+void lcdCursorRight() {
 	lcdCommand(0x14);
 }
-
-
-
 
--- lcd.h
+++ lcd.h
@@ -8,18 +8,21 @@
 #ifndef INC_LCD_H_
 #define INC_LCD_H_
 
-void lcdSetup(GPIO_TypeDef *PORT,GPIO_TypeDef *ENPORT, GPIO_TypeDef *RPORT, uint16_t RS, uint16_t EN, uint16_t D4, uint16_t D5, uint16_t D6, uint16_t D7);
-void lcdEnable (void);
+void lcdSetup(GPIO_TypeDef *PORT, GPIO_TypeDef *ENPORT, GPIO_TypeDef *RPORT,
+		uint16_t RS, uint16_t EN, uint16_t D4, uint16_t D5, uint16_t D6,
+		uint16_t D7);
+void lcdEnable(void);
 void lcdWrite(uint8_t data);
 void lcdCommand(uint8_t command);
 void lcdInit(void);
 void lcdChar(uint8_t ch);
 void lcdSendNibble(uint8_t nibble);
-void lcdString(char * string);
-void lcdWriteInt(char * format, uint32_t number);
-void lcdWriteFloat(char * format, double number);
+void lcdString(char *string);
+void lcdWriteInt(char *format, uint32_t number);
+void lcdWriteFloat(char *format, double number);
 void lcdSetCursor(uint8_t row, uint8_t col);
 void lcdCustomCharacter(uint8_t location, uint8_t charmap[]);
+void lcdPrintBinary(uint32_t number, uint32_t bits);
 void lcdClear(void);
 void lcdCursorOFF(void);
 void lcdCursorON(void);
@@ -28,6 +31,7 @@ void lcdCursorHome(void);
 void lcdShiftRight(void);
 void lcdShiftLeft(void);
 void lcdOFF(void);
+void lcdON(void);
 void lcdCursorLeft(void);
 void lcdCursorRight(void);
 
